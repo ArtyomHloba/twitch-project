01db2d75f3d85e42badfd332d2dcb4aad18980d9
ArtyomHloba <artemgloba2@gmail.com>
2024-10-27 22:23:18 +0200

    Add provider + store to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'; // Импорт Provider
+import store from './store'; // Импорт вашего Redux store
 import './reset.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
