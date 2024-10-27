7887e9f04206704a694667454614b4696d5221ca
ArtyomHloba <artemgloba2@gmail.com>
2024-10-27 21:54:20 +0200

    Add UserList component

--- client/src/pages/UserPage/index.jsx
+++ client/src/pages/UserPage/index.jsx
@@ -1,7 +1,13 @@
+import UsersList from '../../components/UsersList/index.jsx';
 import UserForm from './../../components/forms/UserForm/index.jsx';
 
 function UserPage () {
-  return <UserForm />;
+  return (
+    <>
+      <UserForm />
+      <UsersList />
+    </>
+  );
 }
 
 export default UserPage;
