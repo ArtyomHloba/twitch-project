b35251df64d52eb0ca98d5f3f29500316dfaa240
ArtyomHloba <artemgloba2@gmail.com>
2024-10-27 22:36:55 +0200

    Add controllers for users + http requests

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,3 +1,35 @@
-module.exports.createUser = async (req, res, next) => {};
+const _ = require('lodash');
+const { User } = require('./../models');
 
-module.exports.getUsers = async (req, res, next) => {};
+module.exports.createUser = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const createdUser = await User.create(body);
+    const preparedUser = _.omit(createdUser.get(), ['createdAt', 'updatedAt']);
+    res.status(201).send({ data: preparedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getUsers = async (req, res, next) => {
+  const {
+    query: { page, results },
+  } = req;
+
+  const limit = results;
+  const offset = (page - 1) * results;
+
+  try {
+    const foundUsers = await User.findAll({
+      raw: true,
+      limit,
+      offset,
+      order: ['id'],
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+    });
+    res.status(200).send({ data: foundUsers });
+  } catch (error) {
+    next(error);
+  }
+};
