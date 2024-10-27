c485e490017e82cd5d43dda9381e29e897c3afcd
ArtyomHloba <artemgloba2@gmail.com>
2024-10-27 21:34:26 +0200

    Add seed for create user to db

---
+++ server/seeders/add-user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          nickname: 'Test226',
+          name: 'Test',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
