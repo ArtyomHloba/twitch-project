cc70b56b354b2e7c95c975d2fd7ddbbf1bf868a1
ArtyomHloba <artemgloba2@gmail.com>
2024-10-28 10:56:26 +0200

    Add twith icon + styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import styles from './App.module.css';
+import { FaTwitch } from 'react-icons/fa';
 import UserPage from './pages/UserPage';
 
 function App () {
   return (
     <div className={styles.formContainer}>
-      <h1>Добавить пользователя Twitch</h1>
+      <h1>
+        Добавить пользователя Twitch{' '}
+        <span className={styles.twithIcon}>
+          <FaTwitch />
+        </span>
+      </h1>
       <UserPage />
     </div>
   );
